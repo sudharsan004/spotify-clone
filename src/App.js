149df7a26cb6df0a264978055144aa4206de6475
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-01 14:19:32 +0530

    add Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Login from './components/Login'
+import './App.css'
 function App() {
     return (
-        <div>
-            spotify clone
-        </div>
+        <>
+            <Login />
+        </>
     )
 }
 
